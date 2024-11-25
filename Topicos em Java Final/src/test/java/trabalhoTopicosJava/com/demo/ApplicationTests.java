533eaf6addc426ba6643fb61049c567fca12bb60
GuilhermeGaffuri <150754130+GuilhermeGaffuri@users.noreply.github.com>
2024-11-25 14:00:22 -0300

    TopJava

---
+++ Topicos em Java Final/src/test/java/trabalhoTopicosJava/com/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalhoTopicosJava.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
